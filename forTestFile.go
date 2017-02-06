a5c66c54cbc8e3c965e4b5d5f9c092e018865d81
gloomysw <gloomysw>
2017-02-06 10:30:26 +0800

    增加for循环 测试代码, 判断在for 遍历时追加map元素是否起实际作用   实际会造成for遍历出现不规则运算

--- forTestFile.go
+++ forTestFile.go
@@ -3,10 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	fileStr:=[]string{"1","2","3"}
-	for _, value := range fileStr {
-		fmt.Println(value)
-		return
+	//fileStr:=[]string{"1","2","3"}
+	//for _, value := range fileStr {
+	//	fmt.Println(value)
+	//	break
+	//}
+	//fmt.Println("2")
+
+	var list map[int]string=make(map[int]string)
+
+	list[1]="1"
+	list[2]="1"
+	list[3]="1"
+	list[4]="1"
+	list[5]="1"
+	list[6]="1"
+	list[7]="1"
+	list[8]="1"
+	list[9]="1"
+
+	for index, _ := range list {
+		if index<5 {
+			fmt.Println(index)
+			list[index+10]="2"
+		}
+		//fmt.Println(index)
 	}
-	fmt.Println("2")
+
+	for index, _ := range list {
+		fmt.Println(index)
+	}
+	fmt.Println(len(list))
 }
