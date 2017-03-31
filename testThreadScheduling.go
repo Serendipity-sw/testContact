a93629b9ac09bf2c27a7bebee6f066c15719cbe5
gloomysw <gloomysw>
2017-03-31 10:46:42 +0800

    线程锁测试

--- testThreadScheduling.go
+++ testThreadScheduling.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
@@ -15,4 +18,27 @@ func main() {
 	}()
 	blockObj.Wait()
 	fmt.Println(123)
+	blockObj.Add(1)
+	go threadDownasdf(&blockObj)
+	go threadDownasdf(&blockObj)
+	time.Sleep(10 * time.Second)
+	blockObj.Done()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
+	//信号等待
+	<-c
+}
+
+func threadDownasdf(wg *sync.WaitGroup) {
+	jsTmr := time.NewTimer(1 * time.Minute)
+	go func() {
+		wg.Wait()
+		fmt.Println(5)
+		jsTmr.Stop()
+	}()
+	for {
+		fmt.Println(4)
+		jsTmr.Reset(3 * time.Second)
+		<-jsTmr.C
+	}
 }
