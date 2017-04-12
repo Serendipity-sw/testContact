86aa84078a9b0b71ef546cb91196028690ce7095
gloomysw <shaow@axon.com.cn>
2017-04-12 10:50:30 +0800

    增加ftp获取文件列表测试

--- testFtp.go
+++ testFtp.go
@@ -23,14 +23,24 @@ func main() {
 		fmt.Println(3, " ", err.Error())
 		return
 	}
+
+	nameList, err := c.NameList("")
+	if err != nil {
+		fmt.Println(12)
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(nameList)
+	return
+
 	data := bytes.NewBufferString("nihaobuhaoa")
-	err = c.Stor("gloomyswtestFile", data)
+	err = c.Stor("gloomyswtestFile13", data)
 	if err != nil {
 		fmt.Println("4", " ", err.Error())
 		return
 	}
 	fmt.Println("successfully")
-	err = c.Stor("shaowei", bytes.NewBufferString("fsasdfsadf"))
+	err = c.Stor("shaowei23", bytes.NewBufferString("fsasdfsadf"))
 	if err != nil {
 		fmt.Println("5", " ", err.Error())
 		return
